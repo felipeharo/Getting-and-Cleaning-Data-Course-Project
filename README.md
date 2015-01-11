4e2151a559598bc79bc934ee019acc4fc8d962ae
felipeharo <felipe_haro@hotmail.com>
2015-01-11 20:02:26 -0300

    all the files again

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# Getting-and-Cleaning-Data-Course-Project
+READ ME
+==============================
+
+## Content
+This repository contains:  
+- **codebook.pdf**: The codebook containing the explanation for all the variables present in tidy_data.txt  
+- **tidy_data.txt**: The dataset which is a result of the data processing and cleaning. This is the output file of run_analysis.R  
+- **run_analysis.R**: an R file containing a function that reads the raw data, clean it and process it. The output of this function is a file named tidy_data.txt which contains the tidy data.  
+
+## Preparing your working directory
+To be able to use run_analysis.R you need to clone this repository or download that file into your computer. You will also need to download the raw data. You can download the raw data in the following link:  
+[https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip](https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip)  
+The zip file containing the raw data contains a folder called "UCI HAR Dataset". the run_analysis.R file and the unzipped folder with that exact name must be in the same directory for the function to work. Be sure to make that directory the working directory in RStudio or RConsole to avoid errors. This is done using the setwd() function.
+
+## Running the function
+To use the function you will need to install 3 R packages. You can do this with the following code:
+```
+# Required packages. 
+install.packages("data.table")  
+install.packages("plyr")  
+install.packages("dplyr")  
+```
+Once you have the folder and run_analysis.R in the same folder and set up your working directory if needed, you must source the function to be able to run run_analysis(). This function will load the required packages for you.    
+This function will read all the raw data and output a file called "tidy_data.txt". You can read that data by doing the following 
+```
+tidy_data<-read.table("tidy_data.txt",header=TRUE)
+```
+You can use the tidy_data variable to check how amazingly tidy the data is, following all the principles of tidy data.  
+
+ENJOY!!!
--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,9 @@
-#run_analysis<-function(){
-  # setwd("c3_cp")
-  rm(list=ls())
+# this function reads the samsung data, cleans the data and writes a new file with
+# the average values for the mean and standard deviation measurements
+# the function requires to have the "UCI HAR Dataset" folder with all the raw data
+# in the same directory as this file (this directory beeing the working directory)
+
+run_analysis<-function(){
   library(data.table)
   library(plyr)
   library(dplyr)
@@ -24,25 +27,33 @@
   X_train[,subject:=subject_train]
   X_train[,activity:=Y_train]
   
-  # Merges the training and the test sets to create one data set.
+  # 1. Merges the training and the test sets to create one data set.
   data<-rbind(X_train,X_test)
   
-  # Appropriately labels the data set with descriptive variable names
-  setnames(data,names(data)[1:561],features$V2)
   
-  # Extracts only the measurements on the mean and standard deviation for each measurement.
+  
+  # 2. Extracts only the measurements on the mean and standard deviation for each measurement.
+  setnames(data,names(data)[1:561],features$V2)
   data<-cbind(select(data,contains("mean()")), select(data,contains("std()")),select(data,contains("subject")),select(data,contains("activity")))
   
-  # Uses descriptive activity names to name the activities in the data set
+  # 3. Uses descriptive activity names to name the activities in the data set
   data$activity<-as.factor(data$activity)
   data$activity<-revalue(data$activity, c("1"="WALKING", "2"="WALKING_UPSTAIRS", "3"="WALKING_DOWNSTAIRS", "4"="SITTING", "5"="STANDING", "6"="LAYING"))
-
-  # creates data set with the average of each variable for each activity and each subject.
+  
+  
+  # 4. Appropriately labels the data set with descriptive variable names
+  aux<-gsub("-", "_", names(data)[1:dim(data)[2]])
+  aux<-gsub("log(", "", aux, fixed="TRUE")
+  aux<-gsub("(", "", aux, fixed="TRUE")
+  aux<-gsub(")","",aux)
+  setnames(data,names(data)[1:68],aux) 
+  
+  # 5. creates data set with the average of each variable for each activity and each subject.
   data2<-data %>%
     group_by(subject,activity) %>%
     summarise_each(funs(mean)) 
 
   # write the final table
   write.table(data2,file="tidy_data.txt",row.name=FALSE)
-#}
+}
 
